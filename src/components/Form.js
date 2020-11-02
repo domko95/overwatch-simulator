36cafe1b6e01485eaf26d180ef5c915f3f361291
domko95 <dominique.kolbe1995@googlemail.com>
2020-11-02 19:05:26 +0100

    Add Regions-Buttons

---
+++ src/components/Form.js
@@ -0,0 +1,31 @@
+export default function Form() {
+  return (
+    <details>
+      <summary>Anmeldung</summary>
+      <button type="button" value="bfc">
+        Baden-Württermberg
+      </button>
+      <button type="button" value="apex">
+        Bayern
+      </button>
+      <button type="button" value="ea">
+        Brandenburg, Berlin, Sachsen, Sachsen-Anhalt, Thüringen
+      </button>
+      <button type="button" value="nwn">
+        Bremen, Niedersachsen
+      </button>
+      <button type="button" value="nl">
+        Hamburg, Mecklemburg-Vorpommern, Schleswig-Holstein
+      </button>
+      <button type="button" value="hsr">
+        Hessen, Rheinland-Pfalz, Saarland
+      </button>
+      <button type="button" value="nrw">
+        Nordrhein-Westfalen
+      </button>
+      <button type="button" value="alps">
+        Österreich, Schweiz
+      </button>
+    </details>
+  );
+}
