bcb9636487993d668893aefb76efce0861f936f5
domko95 <dominique.kolbe1995@googlemail.com>
2020-11-02 19:22:39 +0100

    Add handleClick functioN

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,42 @@
+import { useState } from 'react';
+
 export default function Form() {
+  const [region, setRegion] = useState('');
+  const handleClick = (value) => {
+    setRegion(value);
+    localStorage.setItem('region', region);
+  };
   return (
     <details>
-      <summary>Anmeldung</summary>
-      <button type="button" value="bfc">
+      <summary>
+        <h2>Anmeldung</h2>
+      </summary>
+      <button type="button" value="bfc" onClick={() => handleClick('bfc')}>
         Baden-Württermberg
       </button>
-      <button type="button" value="apex">
+      <button type="button" value="apex" onClick={() => handleClick('apex')}>
         Bayern
       </button>
-      <button type="button" value="ea">
+      <button type="button" value="ea" onClick={() => handleClick('ea')}>
         Brandenburg, Berlin, Sachsen, Sachsen-Anhalt, Thüringen
       </button>
-      <button type="button" value="nwn">
+      <button type="button" value="nwn" onClick={() => handleClick('nwn')}>
         Bremen, Niedersachsen
       </button>
-      <button type="button" value="nl">
+      <button type="button" value="nl" onClick={() => handleClick('nl')}>
         Hamburg, Mecklemburg-Vorpommern, Schleswig-Holstein
       </button>
-      <button type="button" value="hsr">
+      <button type="button" value="hsr" onClick={() => handleClick('hsr')}>
         Hessen, Rheinland-Pfalz, Saarland
       </button>
-      <button type="button" value="nrw">
+      <button type="button" value="nrw" onClick={() => handleClick('nrw')}>
         Nordrhein-Westfalen
       </button>
-      <button type="button" value="alps">
+      <button type="button" value="alps" onClick={() => handleClick('alps')}>
         Österreich, Schweiz
+      </button>
+      <button type="button" onClick={() => handleClick('')}>
+        Clear
       </button>
     </details>
   );
